b97fef3667d31717ceb7137ea0db73709eb4e8e4
Hector Perez <hecpeare@gmail.com>
2017-10-23 19:30:42 +0100

    precompile assets

--- public/assets/application-551a6c80a5fa88758fe667624d8f82794217e0a495617afd5366a1bd3009e581.js
+++ public/assets/application-551a6c80a5fa88758fe667624d8f82794217e0a495617afd5366a1bd3009e581.js
@@ -13991,61 +13991,6 @@ function drawSchoolsChart() {
 }
 ;
 (function() {
-  this.GoogleAnalytics = (function() {
-    function GoogleAnalytics() {}
-
-    GoogleAnalytics.load = function() {
-      var firstScript, ga;
-      window._gaq = [];
-      window._gaq.push(["_setAccount", GoogleAnalytics.analyticsId()]);
-      ga = document.createElement("script");
-      ga.type = "text/javascript";
-      ga.async = true;
-      ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-      firstScript = document.getElementsByTagName("script")[0];
-      firstScript.parentNode.insertBefore(ga, firstScript);
-      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-        return document.addEventListener("page:change", (function() {
-          return GoogleAnalytics.trackPageview();
-        }), true);
-      } else {
-        return GoogleAnalytics.trackPageview();
-      }
-    };
-
-    GoogleAnalytics.trackPageview = function(url) {
-      if (!GoogleAnalytics.isLocalRequest()) {
-        if (url) {
-          window._gaq.push(["_trackPageview", url]);
-        } else {
-          window._gaq.push(["_trackPageview"]);
-        }
-        return window._gaq.push(["_trackPageLoadTime"]);
-      }
-    };
-
-    GoogleAnalytics.isLocalRequest = function() {
-      return GoogleAnalytics.documentDomainIncludes("local");
-    };
-
-    GoogleAnalytics.documentDomainIncludes = function(str) {
-      return document.domain.indexOf(str) !== -1;
-    };
-
-    GoogleAnalytics.analyticsId = function() {
-      return 'UA-62041272-1';
-    };
-
-    return GoogleAnalytics;
-
-  })();
-
-  if (window.location.host !== 'localhost') {
-    GoogleAnalytics.load();
-  }
-
-}).call(this);
-(function() {
 
 
 }).call(this);
