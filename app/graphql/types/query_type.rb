9dbea7f0845c27c336a604d8f297af7b42f0904b
Hector Perez <hecpeare@gmail.com>
2017-11-04 10:49:11 +0000

    refactorize graphql query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,30 +1,18 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  field :agreements, types[Types::AgreementType] do
-    description "Votes from individuals and organizations on statements or topics; extent=100 (agree), extent=0 (disagree)"
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
-    argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
-    resolve ->(obj, args, ctx) {
-      Agreement.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
-    }
+  def field_template(field_name, class_name, type_class, desc)
+    field field_name, types[type_class] do
+      description desc
+      argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
+      argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
+      resolve ->(obj, args, ctx) {
+        class_name.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
+      }
+    end
   end
 
-  field :statements, types[Types::StatementType] do
-    description "Topic or statement which can be agreed or disagreed"
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
-    argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
-    resolve ->(obj, args, ctx) {
-      Statement.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
-    }
-  end
-
-  field :individuals, types[Types::IndividualType] do
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
-    argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
-    description "Person or organization who agrees or disagrees"
-    resolve ->(obj, args, ctx) {
-      Individual.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
-    }
-  end
+  field_template(:agreements, Agreement, Types::AgreementType, "Votes from individuals and organizations on statements or topics; extent=100 (agree), extent=0 (disagree)")
+  field_template(:statements, Statement, Types::StatementType, "Topic or statement which can be agreed or disagreed")
+  field_template(:individuals, Individual, Types::IndividualType, "Person or organization who agrees or disagrees")
 end
