565b14a70e57dea81e5db86cb6cb01ef5f76a865
Hector Perez <hecpeare@gmail.com>
2017-11-04 10:22:31 +0000

    new limit for api elements: 100

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,7 +3,7 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :agreements, types[Types::AgreementType] do
     description "Votes from individuals and organizations on statements or topics; extent=100 (agree), extent=0 (disagree)"
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 30].min }
+    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
     argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
     resolve ->(obj, args, ctx) {
       Agreement.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
@@ -12,7 +12,7 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   field :statements, types[Types::StatementType] do
     description "Topic or statement which can be agreed or disagreed"
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 30].min }
+    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
     argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
     resolve ->(obj, args, ctx) {
       Statement.where(["id > ?", args[:after]]).order(id: :asc).limit(args[:limit])
@@ -20,7 +20,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   end
 
   field :individuals, types[Types::IndividualType] do
-    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 30].min }
+    argument :limit, types.Int, default_value: 10, prepare: -> (limit) { [limit, 100].min }
     argument :after, types.Int, default_value: 1, prepare: -> (after) { after }
     description "Person or organization who agrees or disagrees"
     resolve ->(obj, args, ctx) {
