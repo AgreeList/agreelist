3fe429f4a0a648486da0e38d1bac9296667ec9b9
Hector Perez <hecpeare@gmail.com>
2017-11-15 18:21:35 +0000

    use Agreelist instead of AgreeList

--- app/mailers/individual_mailer.rb
+++ app/mailers/individual_mailer.rb
@@ -2,12 +2,12 @@ class IndividualMailer < ActionMailer::Base
   def password_reset(individual)
     @individual = individual
     @base_url = Rails.env.production? ? "http://www.agreelist.org" : "http://www.localhost:3000"
-    mail(to: individual.email, from: "hello@agreelist.org", subject: "AgreeList - Password Reset")
+    mail(to: individual.email, from: "hello@agreelist.org", subject: "Agreelist - Password Reset")
   end
 
   def account_activation(individual)
     @individual = individual
     @base_url = Rails.env.production? ? "http://www.agreelist.org" : "http://www.localhost:3000"
-    mail(to: individual.email, from: "hello@agreelist.org", subject: "AgreeList - Account activation")
+    mail(to: individual.email, from: "hello@agreelist.org", subject: "Agreelist - Account activation")
   end
 end
