70cecd2b3110f62c2158c803cd031983119e22a0
Hector Perez <hecpeare@gmail.com>
2018-01-21 15:25:41 +0000

    specs for home page filters

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -17,6 +17,46 @@ feature "new", js: true do
     end
   end
 
+  context "filter by type" do
+    before do
+      s = create(:statement)
+
+      VCR.use_cassette("wikidata/hawking") do
+        hawking = create(:individual, name: "Stephen Hawking", wikipedia: "https://whatever")
+        create(:agreement, individual: hawking, statement: s, extent: 100)
+      end
+
+      not_on_wp = create(:individual, name: "Someone")
+      create(:agreement, individual: not_on_wp, statement: s, extent: 100)
+      visit "/?min_count=1"
+    end
+
+    scenario "should include only influencers (with Wikipedia page) by default" do
+      expect(page).to have_content("Stephen Hawking")
+      expect(page).not_to have_content("Someone")
+    end
+
+    scenario "should include all people regardless of them having Wikipedia page or not" do
+      within(:css, "span#type-filter") do
+        click_link "influencers"
+      end
+      click_link "all"
+
+      expect(page).to have_content("Stephen Hawking")
+      expect(page).to have_content("Someone")
+    end
+
+    scenario "should only include people with no Wikipedia page" do
+      within(:css, "span#type-filter") do
+        click_link "influencers"
+      end
+      click_link "people with no Wikipedia page"
+
+      expect(page).not_to have_content("Stephen Hawking")
+      expect(page).to have_content("Someone")
+    end
+  end
+
   scenario "filter by occupation" do
     s = create(:statement)
 
