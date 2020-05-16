b1363a48082be63038aa66d5abfbc83c7a5e45e9
Hector Perez <hecpeare@gmail.com>
2020-05-16 20:18:04 +0200

    add state to the game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,19 @@
 class GameController < ApplicationController
+  before_action :load_individual
+
   def index
     @statements = []
-    Statement.order(opinions_count: :desc).limit(10).each do |statement|
+    @individual.statements.order('RANDOM()').limit(10).each do |statement|
       @statements << {
         id: statement.id,
         content: statement.content
       }
     end
+  end
+
+  private
+
+  def load_individual
+    @individual = Individual.where('lower(twitter) = ?', params[:id].downcase).first || Individual.find_by_hashed_id(params[:id].gsub("user-", ""))
   end
 end
--- app/javascript/components/game/GameComponent.tsx
+++ app/javascript/components/game/GameComponent.tsx
@@ -1,21 +1,55 @@
 import * as React from 'react'
+import { Button } from 'react-bootstrap'
 
 interface Statement {
   id: number,
   content: string
 }
 
-interface GameProps {
-  statements: Statement[]
+interface Individual {
+  id: number,
+  name: string
 }
 
-export class GameComponent extends React.Component<GameProps>{
+interface GameProps {
+  statements: Statement[],
+  individual: Individual
+}
+
+interface GameState {
+  answers: number[] // 100 agree, 50 skip, 0 disagree
+  currentQuestion: number
+}
+
+export class GameComponent extends React.Component<GameProps, GameState>{
+  constructor(props: GameProps) {
+    super(props)
+
+    this.state = {
+      answers: [],
+      currentQuestion: 0
+    }
+  }
+
+  vote = (answer: number) => {
+    const { answers, currentQuestion } = this.state
+    answers[currentQuestion] = answer
+    this.setState({ answers: answers, currentQuestion: currentQuestion + 1 })
+  }
+
   render() {
+    const { statements, individual } = this.props
+    const { currentQuestion } = this.state
+
     return (
       <div>
-        <p>Hi {this.props.statements[0].content}!</p>
-        <a href="/ddd" className="btn btn-primary">Agree</a>
-        <a href="/ddd" className="btn btn-primary">Disagree</a>
+        <h1>How much do you agree with {individual.name}?</h1>
+        <h5>Vote {statements.length} statements to find out! At the end we'll let you know which statements {individual.name} agrees.</h2>
+        <h2>{statements[currentQuestion].content}</h2>
+        <p>Do you agree?</p>
+        <Button variant="success" onClick={() => this.vote(100)}>Agree</Button>
+        <Button variant="danger" onClick={() => this.vote(0)}>Disagree</Button>
+        <Button variant="link" onClick={() => this.vote(50)}>Skip</Button>
       </div>
     )
   }
