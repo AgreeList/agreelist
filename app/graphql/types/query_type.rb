121a9c69fe90bcc8569f4b814ac0157f5daa8840
Hector Perez <hecpeare@gmail.com>
2020-05-17 15:25:33 +0200

    restore graphql pagination

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,26 +2,32 @@ module Types
   class QueryType < Types::BaseObject
     field :statements, [Types::StatementType], null: false do
       description "Topic or statement which can be agreed or disagreed"
+      argument :limit, Integer, required: false, default_value: 10, prepare: -> (limit, ctx) { [limit, 100].min }
+      argument :after, Integer, required: false, default_value: 0
     end
 
     field :agreements, [Types::AgreementType], null: false do
       description "Votes from individuals and organizations on statements or topics; extent=100 (agree), extent=0 (disagree)"
+      argument :limit, Integer, required: false, default_value: 10, prepare: -> (limit, ctx) { [limit, 100].min }
+      argument :after, Integer, required: false, default_value: 0
     end
 
     field :individuals, [Types::IndividualType], null: false do
       description "Person or organization who agrees or disagrees"
+      argument :limit, Integer, required: false, default_value: 10, prepare: -> (limit, ctx) { [limit, 100].min }
+      argument :after, Integer, required: false, default_value: 0
     end
 
-    def statements
-      Statement.limit(10)
+    def statements(limit:, after:)
+      Statement.limit(limit).where("id > ?", after)
     end
 
-    def individuals
-      Individual.limit(10)
+    def individuals(limit:, after:)
+      Individual.limit(limit).where("id > ?", after)
     end
 
-    def agreements
-      Agreement.limit(10)
+    def agreements(limit:, after:)
+      Agreement.limit(limit).where("id > ?", after)
     end
   end
 end
