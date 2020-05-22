0a51699758f63b3ba92098fda78913252234fb81
Hector Perez <hecpeare@gmail.com>
2020-05-22 17:23:58 +0200

    render answer

--- app/javascript/components/game/GameComponent.tsx
+++ app/javascript/components/game/GameComponent.tsx
@@ -28,7 +28,8 @@ interface GameProps {
 
 interface GameState {
   answers: number[], // 100 agree, 50 skip, 0 disagree
-  currentQuestion: number
+  currentQuestion: number,
+  showAnswer: boolean
 }
 
 export class GameComponent extends React.Component<GameProps, GameState>{
@@ -37,15 +38,16 @@ export class GameComponent extends React.Component<GameProps, GameState>{
 
     this.state = {
       answers: [],
-      currentQuestion: 0
+      currentQuestion: 0,
+      showAnswer: false
     }
   }
 
   vote = (answer: number) => {
-    const { answers, currentQuestion } = this.state
+    const { answers, currentQuestion, showAnswer } = this.state
     const { individual, agreements } = this.props
     answers[currentQuestion] = answer
-    this.setState({ answers: answers, currentQuestion: currentQuestion + 1 })
+    this.setState({ answers: answers, currentQuestion: currentQuestion + 1, showAnswer: !showAnswer })
     const event_args = {
       name: "vote",
       statement_id: agreements[currentQuestion].statement.id,
@@ -56,27 +58,46 @@ export class GameComponent extends React.Component<GameProps, GameState>{
     axios.post('/api/v2/events', event_args)
   }
 
-  render() {
-    const { agreements, individual } = this.props
-    const { currentQuestion } = this.state
-
-    const count = agreements.length
+  renderQuestion = () => {
     return (
-      <div>
-        <div className="game-wrap">
-          <div className="game-picture">
-            <img src={individual.picture_url} alt={`${individual.name} photo`} />
-          </div>
-          <div className="game-question">
-            <h1>Do you agree with {individual.name}?</h1>
-          </div>
-        </div>
-        <h5>Vote to see {individual.name}'s opinions:</h5>
-        <h2>{currentQuestion + 1}. {agreements[currentQuestion].statement.content}</h2>
+      <>
         <p>Do you agree?</p>
         <Button variant="success" className="game-agree" onClick={() => this.vote(100)}>Agree</Button>
         <Button variant="danger" onClick={() => this.vote(0)}>Disagree</Button>
         <Button variant="link" onClick={() => this.vote(50)}>Skip</Button>
+      </>
+    )
+  }
+
+  renderAnswer = () => {
+    const { agreements, individual } = this.props
+    const { currentQuestion } = this.state
+    const agreement = agreements[currentQuestion]
+    const reason = agreement.reason
+
+    const agreesOrDisagrees = agreement.extent == 100 ? 'agrees' : 'disagrees'
+
+    return (
+      <>
+        {individual.name} agrees:
+        <div className="opinion">
+          <i>{reason}</i>
+        </div>
+        <Button variant="primary" onClick={() => this.setState({ currentQuestion: currentQuestion + 1, showAnswer: false})}>Next question</Button>
+      </>
+    )
+  }
+
+  render() {
+    const { agreements, individual } = this.props
+    const { showAnswer, currentQuestion } = this.state
+
+    const count = agreements.length
+    return (
+      <div>
+        <h5>Vote to see {individual.name}'s opinions:</h5>
+        <h2>{agreements[currentQuestion].statement.content}</h2>
+        {showAnswer ? this.renderAnswer() : this.renderQuestion()}
       </div>
     )
   }
