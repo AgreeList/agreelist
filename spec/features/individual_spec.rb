2c6a5d26fa4e6666d6c513ab5556bd4e743bf6a6
Hector Perez <hecpeare@gmail.com>
2020-05-23 15:03:52 +0200

    improve game spec

--- spec/features/individual_spec.rb
+++ spec/features/individual_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
-feature 'create statement from individual profile', js: true do
-  before do
-    login
-    i = create(:individual, name: "Elon Musk", twitter: "elonmusk")
-    s = create(:statement, individual: i)
-    visit "/auth/twitter"
-    @hector = Individual.find_by_twitter("arpahector")
-    create(:agreement, statement: s, individual: i, extent: 100, added_by_id: @hector.id)
-  end
+feature 'individual page', js: true do
+  context 'create statement from individual profile' do
+    before do
+      login
+      i = create(:individual, name: "Elon Musk", twitter: "elonmusk")
+      s = create(:statement, individual: i)
+      visit "/auth/twitter"
+      @hector = Individual.find_by_twitter("arpahector")
+      create(:agreement, statement: s, individual: i, extent: 100, added_by_id: @hector.id)
+    end
 
-  scenario "should add the creator" do
-    visit "/elonmusk"
-    fill_in :content, with: "We should go to Mars"
-    click_button "She/he agrees"
-    expect(Statement.last.individual.name).to eq "Hector Perez"
-  end
+    scenario "should add the creator" do
+      visit "/elonmusk"
+      fill_in :content, with: "We should go to Mars"
+      click_button "She/he agrees"
+      expect(Statement.last.individual.name).to eq "Hector Perez"
+    end
 
-  context "non-existent individual" do
-    scenario "should return an error page" do
-      visit "/non-existent-page"
-      expect(page).to have_content("Agreelist does not have a page for non-existent-page")
+    context "non-existent individual" do
+      scenario "should return an error page" do
+        visit "/non-existent-page"
+        expect(page).to have_content("Agreelist does not have a page for non-existent-page")
+      end
     end
   end
 
@@ -39,6 +41,16 @@ feature 'create statement from individual profile', js: true do
       visit "/neil"
       click_button "Agree"
       expect(page).to have_content("Neil Armstrong agrees:")
+      click_button "Next question"
+      click_button "Agree"
+      click_button "Next question"
+      click_button "Agree"
+      click_button "Next question"
+      fill_in :email, with: "hec7@hec.com"
+      click_button "Sign up and save progress"
+      expect(page).to have_content("The sky is blue")
+      expect(page).to have_content("Mars is red")
+      expect(page).to have_content("The Moon is yellow")
     end
   end
 end
