c40401402e9f72ed6ed425a373ad0b3a79cc9199
Hector Perez <hecpeare@gmail.com>
2020-05-23 17:10:24 +0200

    fix spec

--- spec/features/individual_spec.rb
+++ spec/features/individual_spec.rb
@@ -29,15 +29,15 @@ feature 'individual page', js: true do
   context "game" do
     before do
       i = create(:individual, name: "Neil Armstrong", twitter: "neil")
-      s1 = create(:statement, content: "The sky is blue")
-      s2 = create(:statement, content: "Mars is red")
-      s3 = create(:statement, content: "The Moon is yellow")
-      create(:agreement, statement: s1, individual: i, extent: 100)
-      create(:agreement, statement: s2, individual: i, extent: 100)
-      create(:agreement, statement: s3, individual: i, extent: 100)
+      @s1 = create(:statement, content: "The sky is blue")
+      @s2 = create(:statement, content: "Mars is red")
+      @s3 = create(:statement, content: "The Moon is yellow")
+      create(:agreement, statement: @s1, individual: i, extent: 100)
+      create(:agreement, statement: @s2, individual: i, extent: 100)
+      create(:agreement, statement: @s3, individual: i, extent: 100)
     end
 
-    scenario "should vote" do
+    scenario "should ask for email when there are no more questions" do
       allow_any_instance_of(ActionController::Base).to receive(:protect_against_forgery?).and_return(true)
       visit "/neil"
       click_button "Agree"
@@ -48,10 +48,12 @@ feature 'individual page', js: true do
       click_button "Agree"
       click_button "Next question"
       fill_in :email, with: "hec7@hec.com"
-      click_button "Sign up and save progress"
+      expect { click_button "Sign up and save progress" }.to change { Individual.count }.by(1)
       expect(page).to have_content("The sky is blue")
       expect(page).to have_content("Mars is red")
       expect(page).to have_content("The Moon is yellow")
+      expect(Individual.last.email).to eq "hec7@hec.com"
+      expect(Individual.last.agreements.pluck(:statement_id).sort).to eq [@s1.id, @s2.id, @s3.id].sort
     end
   end
 end
