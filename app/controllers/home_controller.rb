8e99f3ee5b06fc1182097a17b3d0152b9fc68663
Hector Perez <hecpeare@gmail.com>
2020-06-16 18:50:38 +0200

    do not show links to well known people in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,5 @@ class HomeController < ApplicationController
   def index
     s = ["AI and robots might cause mass unemployment", "AI will pose a serious risk to society within the next 50 years", "Social media threaten democracy", "Basic Income", "Carbon Tax"]
     @statements = Statement.where(content: s).order("LENGTH(content) DESC")
-    i = ["Bill Gates", "Elon Musk", "Yuval Noah Harari", "Barack Obama", "Donald J. Trump"]
-    @individuals = Individual.where(name: i).order(opinions_count: :desc)
   end
 end
